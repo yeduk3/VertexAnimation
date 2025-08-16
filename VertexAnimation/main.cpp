963757eda5385a7690e6c0aa86cf457cf5fb4c54
yeduk3 <yeduk33@gmail.com>
2025-08-16 23:59:34 +0900

    Initial Commit

---
+++ VertexAnimation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VertexAnimation
+//
+//  Created by 이용규 on 8/16/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
